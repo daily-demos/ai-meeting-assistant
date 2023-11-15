f33e30145260db532ce92468df9eddcb2cc69a29
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-11-15 18:27:19 +0100

    merge client and update readme

---
+++ client/pages/api/create-session.js
@@ -0,0 +1,25 @@
+const createSessionBackend = `http://daily-ai-assistant-wip4.eba-khngcrah.us-east-2.elasticbeanstalk.com/session`;
+
+export default async function handler(req, res) {
+  const response = await fetch(createSessionBackend, {
+    headers: {
+      "Content-type": "application/json",
+    },
+    method: "POST",
+  });
+
+  if (!response.ok) {
+    res.status(500).json({
+      error: "Error when creating session",
+      details: await response.json(),
+    });
+    return;
+  }
+  const body = await response.json();
+
+  if (response.ok) {
+    res.status(200).json({ url: body.room_url });
+  } else {
+    res.status(500).json({ error: "Error when creating room", details: body });
+  }
+}
