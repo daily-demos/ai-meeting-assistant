da370d98ad401d7e1d5e35859573e1ad2b41de6d
Liza <65890040+lazeratops@users.noreply.github.com>
2024-01-08 10:16:57 +0100

    Support for different Daily envs (#17)
    * Support for different Daily envs
    
    * rename
    
    * Fix session iteration on shutdown

--- server/config.py
+++ server/config.py
@@ -5,15 +5,39 @@ from __future__ import annotations
 import os
 
 
+class DailyAPIData:
+    """Class representing a Daily API key and its associated
+    domain and environment."""
+    _domain: str
+    _env: str
+    _key: str
+
+    def __init__(self, domain: str, env: str, key: str):
+        self._domain = domain
+        self._env = env
+        self._key = key
+
+    def get_api_url(self) -> str:
+        """Returns the API URL for this environment."""
+        if not self._env:
+            # If env was not provided, assume production
+            return f"https://api.daily.co/v1/"
+        return f"https://api.{self._env}.daily.co/v1/"
+
+    @property
+    def key(self) -> str:
+        return self._key
+
+
 class Config:
     """Class representing third-party API keys and other settings."""
-    _daily_api_key: str = None
-    _daily_api_url: str = None
+    _default_daily_api_key: str = None
+    _default_daily_api_url: str = None
     _openai_api_key: str = None
     _openai_model_name: str = None
     _room_duration_mins: int = None
     _log_dir_path: str = None
-    _daily_api_keys: dict[str, str] = None
+    _daily_api_keys: dict[str, DailyAPIData] = None
 
     def __init__(self, daily_api_key=os.getenv("DAILY_API_KEY"),
                  daily_api_url=os.getenv("DAILY_API_URL"),
@@ -22,13 +46,13 @@ class Config:
                  room_duration_mins=os.getenv("ROOM_DURATION_MINUTES"),
                  log_dir_name: str = None
                  ):
-        self._daily_api_key = daily_api_key
+        self._default_daily_api_key = daily_api_key
         self._openai_api_key = openai_api_key
         self._openai_model_name = openai_model_name
 
         if not daily_api_url:
             daily_api_url = 'https://api.daily.co/v1'
-        self._daily_api_url = daily_api_url
+        self._default_daily_api_url = daily_api_url
 
         if not room_duration_mins:
             room_duration_mins = 15
@@ -41,27 +65,37 @@ class Config:
         """Loads all Daily API keys from environment variables"""""
         self._daily_api_keys = {}
         items = os.environ.items()
+        key_prefix = "DAILY_API_KEY_"
         for key, value in items:
-            if key.startswith("DAILY_API_KEY_"):
-                domain_name = key.split("DAILY_API_KEY_")[1].lower()
-                self._daily_api_keys[domain_name] = value
+            if key.startswith(key_prefix):
+                domain_and_env = key.split(key_prefix)[1].lower()
+                parts = domain_and_env.split("_")
 
-    def get_daily_api_key(self, domain_name: str) -> str:
-        """Returns the Daily API key for the given domain name if one exists"""
+                domain_name: str = parts[0]
+                env_name: str = None
+
+                # If there are two parts, the second is the environment name
+                if len(parts) == 2:
+                    env_name = parts[1]
+                api_key = DailyAPIData(domain_name, env_name, value)
+                self._daily_api_keys[domain_name] = api_key
+
+    def get_daily_api_data(self, domain_name: str) -> DailyAPIData | None:
+        """Returns the Daily API data for the given domain name if it exists"""
         if self._daily_api_keys is None:
             self.load_daily_api_keys()
         dn = domain_name.lower()
         return self._daily_api_keys.get(dn)
 
     @property
-    def daily_api_key(self) -> str:
+    def default_daily_api_key(self) -> str:
         """Returns the configured Daily API key"""
-        return self._daily_api_key
+        return self._default_daily_api_key
 
     @property
-    def daily_api_url(self) -> str:
+    def default_daily_api_url(self) -> str:
         """Returns the configured Daily API URL"""
-        return self._daily_api_url
+        return self._default_daily_api_url
 
     @property
     def openai_api_key(self) -> str:
